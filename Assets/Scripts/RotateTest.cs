695f8922ab82ba76d96e3dfa4a9aaa3b25020932
Marwan Osama <MarwanOsama1997@gmail.com>
2019-10-03 16:32:07 +0200

    vvss tutorial

---
+++ Assets/Scripts/RotateTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTest : MonoBehaviour
+{
+    public void RotateObject(GameObject mGameObject)
+    {
+        mGameObject.transform.eulerAngles += new Vector3(0f, 0f,10f);
+    }
+}
